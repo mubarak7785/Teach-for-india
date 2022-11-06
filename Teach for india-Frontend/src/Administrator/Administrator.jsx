b128fbbc9c6c976605a80aa61a6d96b7a5a0394c
mubarak7785 <mubarakjinabade07@gmail.com>
2022-11-06 13:10:56 +0530

    css done

--- Teach for india-Frontend/src/Administrator/Administrator.jsx
+++ Teach for india-Frontend/src/Administrator/Administrator.jsx
@@ -14,22 +14,26 @@ export const Administrator = () => {
       <Navbar />
       <div className="adminis-div">
         <div className="head">
-          <p>Name</p>
-          <p>Age</p>
-          <p>Phone-No</p>
-          <p>Location</p>
-          <p>Language</p>
-          <p>Day</p>
+            <div><p>Name</p></div>
+            <div> <p>Age</p></div>
+            <div><p>Phone-No</p></div>
+            <div> <p>Location</p></div>
+            <div> <p>Language</p></div>
+            <div><p>Day</p></div> 
         </div>
         {data.map((e) => {
           return (
             <div className="head">
-              <p>{e.full_name}</p>
-              <p>{e.age}</p>
-              <p>{e.phone}</p>
-              <p>{e.location}</p>
-              <p>{e.spoken_languages}</p>
-              <p>{e.availability}</p>
+                <div> <p>{e.full_name}</p></div>
+                <div> <p>{e.age}</p></div>
+                <div> <p>{e.phone}</p></div>
+                <div><p> {e.location}</p></div>
+                <div><p>{e.spoken_languages}</p></div>
+                <div><p>{e.availability}</p></div>
+             
+           
+              
+              
             </div>
           );
         })}
